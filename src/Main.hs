16d922f8ef1abb5be47ee9565b336a4fe72f66e1
Stephen Weeks <sweeks@imvu.com>
2015-10-24 01:32:37 -0700

    Run commands, but no live updates yet

--- src/Main.hs
+++ src/Main.hs
@@ -7,6 +7,7 @@ import Control.Concurrent
 import Control.Concurrent.Chan
 import Control.Lens
 import Control.Monad (void)
+import Control.Monad.IO.Class (liftIO)
 import Data.Default
 import GHC.IO.Exception (ExitCode(..))
 import qualified Graphics.Vty as Vty
@@ -20,7 +21,9 @@ import Brick.Types
   ( Widget
   )
 import Brick.Widgets.Core
-  ( str
+  ( vBox
+  , str
+  , viewport
   , (<=>)
   )
 
@@ -29,25 +32,26 @@ import System.IO
   ( hGetContents
   )
 
--- XXX TODO replace the List with a viewport of Historys
-data Weaver =
-  Weaver { _input :: E.Editor
-         , _history :: L.List String
-         }
-
-makeLenses ''Weaver
+data WeaverEvent =
+  VtyEvent Vty.Event
+  | CommandOutput Int String
+  | CommandFinished Int ExitCode
 
 data History =
   History { _returnValue :: Maybe ExitCode
-          , _output :: [String]
+          , _output :: String
           }
 
 makeLenses ''History
 
-data WeaverEvent =
-  VtyEvent Vty.Event
-  | CommandOutput [String]
-  | CommandFinished ExitCode
+-- XXX TODO replace the List with a viewport of Historys
+data Weaver =
+  Weaver { _input :: E.Editor
+         , _history :: [History]
+         , _eventChannel :: Chan WeaverEvent
+         }
+
+makeLenses ''Weaver
 
 historyName :: T.Name
 historyName = "history"
@@ -59,38 +63,58 @@ mainUI :: Weaver -> [Widget]
 mainUI w = [ui]
   where
     ui = _history <=> i
-    _history = L.renderList (w ^. history) renderHistoryElement
+    _history = viewport historyName T.Vertical $ vBox $ map renderHistoryElement (w ^. history)
     i = E.renderEditor $ w ^. input
 
-renderHistoryElement :: Bool -> String -> Widget
-renderHistoryElement _ i = str i
+renderHistoryElement :: History -> Widget
+renderHistoryElement h = str $ h ^. output
 
 histScroll :: M.ViewportScroll
 histScroll = M.viewportScroll historyName
 
 weaverEvent :: Weaver -> WeaverEvent -> T.EventM (T.Next Weaver)
 weaverEvent w (VtyEvent v) = appEvent w v
+weaverEvent w (CommandOutput i t) = M.continue $ w & (history . ix i . output) %~ (++ t)
+weaverEvent w (CommandFinished i rv) = M.continue $ w & (history . ix i . returnValue) .~ Just rv
 
 appEvent :: Weaver -> Vty.Event -> T.EventM (T.Next Weaver)
 appEvent w (Vty.EvKey Vty.KDown [])  = M.vScrollBy histScroll 1 >> M.continue w
 appEvent w (Vty.EvKey Vty.KUp [])    = M.vScrollBy histScroll (-1) >> M.continue w
-appEvent w (Vty.EvKey Vty.KEnter []) = M.continue $ runCommand w
+appEvent w (Vty.EvKey Vty.KEnter []) = forkRunCommand w >>= M.continue
 appEvent w (Vty.EvKey Vty.KEsc []) = M.halt w
 appEvent w ev = T.handleEventLensed w input ev >>= M.continue
+
+launchShellProcess cmd =  P.createProcess (P.shell cmd){P.std_out = P.CreatePipe, P.std_in = P.CreatePipe}
+
+forkRunCommand :: Weaver -> T.EventM Weaver
+forkRunCommand w = do
+  pid <- liftIO $ forkIO $ do
+    (_, so, _, h) <- launchShellProcess t
+    rv <- P.waitForProcess h
+    out <- maybe (return "") hGetContents so
+    writeChan (w ^. eventChannel) (CommandOutput i out)
+    writeChan (w ^. eventChannel) (CommandFinished i rv)
+  return emptied
+  where
+    appended = w & history %~ (++ [ History Nothing t ])
+    emptied = appended & input .~ emptyInput
+    i = length ( w ^. history)
+    t = unlines $ E.getEditContents $ w ^. input
 
 runCommand :: Weaver -> Weaver
 runCommand w = emptied
   where
-    appended = w & history %~ (L.listInsert i t)
+    appended = w & history %~ (++ [ History Nothing t ])
     emptied = appended & input .~ emptyInput
-    i = Vector.length $ w ^. history ^. L.listElementsL
     t = unlines $ E.getEditContents $ w ^. input
 
 emptyInput :: E.Editor
 emptyInput = E.editor inputName (str . unlines) (Just 1) ""
 
-initialState :: Weaver
-initialState = Weaver emptyInput (L.list historyName (Vector.fromList ["ohai","lol"]) 1)
+initialState :: IO Weaver
+initialState = do
+  ec <- Control.Concurrent.newChan
+  return $ Weaver emptyInput [] ec
 
 appCursor :: Weaver -> [T.CursorLocation] -> Maybe T.CursorLocation
 appCursor _ = M.showCursorNamed inputName
@@ -105,18 +129,14 @@ app =
           , M.appChooseCursor = appCursor
           }
 
-forkRunCommand :: Chan (ExitCode, String) -> P.CreateProcess -> IO ThreadId
-forkRunCommand chan cmd = forkIO $ do
-  (_, so, _, h) <- P.createProcess cmd
-  rv <- P.waitForProcess h
-  out <- maybe (return "") hGetContents so
-  writeChan chan (rv, out)
-
 main :: IO ()
--- main = void $ M.defaultMain app initialState
 main = do
-  finished <- newChan
-  let cmd = (P.shell "cat /etc/hosts"){P.std_out = P.CreatePipe, P.std_in = P.CreatePipe}
-  _ <- forkRunCommand finished cmd
-  rv <- readChan finished
-  print rv
+  is <- initialState
+  finalState <- M.customMain (Vty.mkVty Data.Default.def) (is ^. eventChannel) app is
+  print "done"
+-- main = do
+--   finished <- newChan
+--   let cmd = (P.shell "cat /etc/hosts"){P.std_out = P.CreatePipe, P.std_in = P.CreatePipe}
+--   _ <- forkRunCommand finished cmd
+--   rv <- readChan finished
+--   print rv
